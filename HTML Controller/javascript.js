ca049a40188f9896a08e26fac147ea1218d3246f
vishwapinnawala <45973257+vishwapinnawala@users.noreply.github.com>
2021-08-11 17:21:34 +0530

    Add files via upload

---
+++ HTML Controller/javascript.js
@@ -0,0 +1,84 @@
+function stop()
+          {
+   var Requests = new XMLHttpRequest();
+   Requests.open("GET", "http://192.168.4.1/?State=S", false);
+   Requests.send(null);
+          }
+function right()
+          {
+   var Requests = new XMLHttpRequest();
+   Requests.open("GET", "http://192.168.4.1/?State=R", false);
+   Requests.send(null);
+          }
+function left()
+          {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=L", false);
+ Requests.send(null);
+          }
+function reverse()
+          {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=B", false);
+  Requests.send(null);
+          }
+function forward()
+          {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=F", false);
+  Requests.send(null);
+          }
+function aheadleft()
+          {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=G", false);
+  Requests.send(null);
+          }
+function aheadright()
+         {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=I", false);
+  Requests.send(null);
+          }
+function backleft()
+          {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=G", false);
+  Requests.send(null);
+          }
+  function backright()
+         {
+  var Requests = new XMLHttpRequest();
+  Requests.open("GET", "http://192.168.4.1/?State=I", false);
+  Requests.send(null);
+         }
+   function speed1()
+      {
+var Requests = new XMLHttpRequest();
+Requests.open("GET", "http://192.168.4.1/?State=1", false);
+Requests.send(null);
+      }
+   function speed2()
+     {
+var Requests = new XMLHttpRequest();
+Requests.open("GET", "http://192.168.4.1/?State=2", false);
+Requests.send(null);
+     }
+  function speed3()
+    {
+var Requests = new XMLHttpRequest();
+Requests.open("GET", "http://192.168.4.1/?State=3", false);
+Requests.send(null);
+    }
+   function speed4()
+   {
+var Requests = new XMLHttpRequest();
+Requests.open("GET", "http://192.168.4.1/?State=4", false);
+Requests.send(null);
+    }
+   function speed5()
+    {
+ var Requests = new XMLHttpRequest();
+ Requests.open("GET", "http://192.168.4.1/?State=5", false);
+ Requests.send(null);
+    }
